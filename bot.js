8a8127115e0cbd618fd717177b996a8a076d65ce
leagueteam9991 <42578947+leagueteam9991@users.noreply.github.com>
2018-08-21 16:23:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-  
-  client.on('message',rebel =>   {
+
+client.on('message',rebel =>   {
  var prefix = "-";
     if(rebel.author.bot) return;
   
     if(rebel.content.startsWith(prefix + "طلب")) {
           let args = rebel.content.split(' ').slice(1).join(' ');
-      if(!args) return rebel.channel.send("** الرجآء كتآبة طلبك **");
+      if(!args) return rebel.channel.send("**آكتب طلبك لو سمحت**");
    rebel.delete(5000);
   
   rebel.channel.send(`محتوى الطلب   
   \n\`${args}\`
   
- ** لو تريد آكمآل طلبك آضغط على علامة صح **
- ** لو تريد آيقآف الطلب و عدم الآكمال آضغط على آلغآء **
+  ** لو تبي تكمل طلبتك آضغط عالصح **
+  ** لو تبي تلغيهآ آضغط عالخطآ **
       `).then(msg => {
      msg.react('❌')
      .then(() => msg.react('✅')).then((collected) => {
@@ -29,12 +29,12 @@ const client = new Discord.Client();
       let no = msg.createReactionCollector(rebel2Filter, {maxMatches : 1,time : 15000,});
   
       yes.on("collect", r => {
-  rebel.guild.channels.get("477956643594960896").send(`
-  ** طلب جديد ! **
-  ------------,------------
+  rebel.guild.channels.get("479450824003682304").send(`
+  __طلبية جديدة__
+  ========================
   **${args}**
-  ------------,------------
-  ** الطلب من طرف ** : ${rebel.author}`).then((dd) =>{
+  ========================
+  تم الطلب بواسطة : ${rebel.author}`).then((dd) =>{
   dd.react("✅")
   dd.react("❌")
   })
@@ -42,9 +42,11 @@ const client = new Discord.Client();
          
    no.on("collect", r => {
           msg.delete();
-          rebel.channel.send(`** تم آلغآء طلبك **`).then(m => m.delete(5000));
-		 client.user.setGame(`-طلب`,"http://twitch.tv/S-F")
- })})})}});
+          rebel.channel.send(`تم إلغآء إرسآل الطلب`).then(m => m.delete(5000));
+   
+       client.user.setGame(`-طلب`,"http://twitch.tv/S-F")
+  
+   })})})}});
 		  
 		  
 client.login(process.env.BOT_TOKEN);
